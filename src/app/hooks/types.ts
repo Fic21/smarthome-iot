ec8b5df0c3b07bbc0bf1f8cbb633d15f99225e02
Fic <ficra725@gmail.com>
2025-06-05 17:01:57 +0700

    memperbaiki dengan menambahkan kolom dtypes di schema.prisma dan menyamai interface pada useDeviceManager

--- src/app/hooks/types.ts
+++ src/app/hooks/types.ts
@@ -1,9 +1,13 @@
 export interface Device {
-  userId?:number;
   deviceId: string;
   name: string;
   topic: string;
   type: "subscriber" | "publisher";
-  category?: string;
+  category: string;
   inputtambahan?: string[];
+  userId: number;
+  mqttBrokerUrl?: string;
+  mqttBrokerPort?: number;
+  mqttToken?: string;
+  mqttTokenExpiry?: string;
 }
